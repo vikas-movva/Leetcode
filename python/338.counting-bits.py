3fbeb40977500d4954cc9f16b329cc45385b1800
Vikas Movva <vikas.s.movva@gmail.com>
2026-08-30 23:46:14 -0400

    Refactor countBits function to simplify return value and remove unnecessary list

--- python/338.counting-bits.py
+++ python/338.counting-bits.py
@@ -8,16 +8,13 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
         dp = [0] * (n+1)
-        ans = [0]
         sig = 1
         
         for i in range(1, n+1):
             if sig*2 == i:
                 sig = i
-        
-            dp[i] = dp[i-sig]+1
-            ans.append(dp[i])
-         
-        return ans
+            dp[i] = 1 + dp[i - sig]
+            
+        return dp
 # @lc code=end
 
