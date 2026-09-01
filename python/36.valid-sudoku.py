593d575ad281ddb6a274e94181dc9f5abaa73fd8
Vikas Movva <vikas.s.movva@gmail.com>
2026-08-31 21:53:33 -0400

    Fix index range in isValidSudoku method for correct Sudoku validation

--- python/36.valid-sudoku.py
+++ python/36.valid-sudoku.py
@@ -7,21 +7,21 @@
 # @lc code=start
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        rows = [set() for _ in range(10)]
-        cols = [set() for _ in range(10)]
-        grid = [set() for _ in range(10)]
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        grid = [set() for _ in range(9)]
         
         for i in range(9):
             for j in range(9):
                 cell = board[i][j]
-                if board[i][j] == ".": continue
+                if cell == ".": continue
                 
                 grid_idx = (j // 3) + ((i // 3) * 3)
                 if cell in rows[i] or cell in cols[j] or cell in grid[grid_idx]:
                     return False
-                rows[i].add(board[i][j])
-                cols[j].add(board[i][j])
-                grid[grid_idx].add(board[i][j])
+                rows[i].add(cell)
+                cols[j].add(cell)
+                grid[grid_idx].add(cell)
                 
         return True
 # @lc code=end
