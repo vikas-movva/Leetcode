275998c77b0597812a27856a5e6fdd559ef8089a
Vikas Movva <vikas.s.movva@gmail.com>
2026-08-31 23:49:22 -0400

    Add solution for LeetCode problem 143: Reorder List

---
+++ python/143.reorder-list.py
@@ -0,0 +1,46 @@
+#
+# @lc app=leetcode id=143 lang=python3
+#
+# [143] Reorder List
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        """
+        Do not return anything, modify head in-place instead.
+        """
+        if not head or not head.next:
+            return head
+        
+        # find middle of list
+        sp = fp = head
+        
+        while fp.next and fp.next.next:
+            sp = sp.next
+            fp = fp.next.next
+            
+        # sp is now middle
+        # reverse second half of list 
+        # and cut off sp
+        prev, curr, sp.next = None, sp.next, None
+        while curr:
+            next, curr.next, prev, curr = curr.next, prev, curr, curr.next
+            curr = next
+        
+        # merge two lists
+        l1, l2 = head, prev
+        while l2:
+            n1, n2 = l1.next, l2.next
+            l1.next = l2
+            l2.next = n1
+            l1, l2 = n1, n2
+            
+        return head   
+# @lc code=end
+    
