ce74cd62ddfe5802c5f10e8f311b0cdb4286bebc
Vikas Movva <vikas.s.movva@gmail.com>
2026-09-01 16:38:33 -0400

    Add comments to vector addition kernel

--- leetgpu/1-vector-addition/triton/solution.py
+++ leetgpu/1-vector-addition/triton/solution.py
@@ -9,16 +9,26 @@ import triton.language as tl
 
 @triton.jit
 def vector_add_kernel(a, b, c, n_elements, BLOCK_SIZE: tl.constexpr):
+    # get program id
     pid = tl.program_id(0)
+    
+    # compute the start index for this block
     block_start = pid * BLOCK_SIZE
+    
+    # offset is a list of pointers to the elements in the block
     offset = block_start + tl.arange(0, BLOCK_SIZE)
     
+    # mask to guard oob memory access
     mask = offset < n_elements
     
+    # load the elements in the block
     x = tl.load(a + offset, mask=mask)
     y = tl.load(b + offset, mask=mask)
+    
+    # compute add
     out  = x + y
     
+    # store the result in c
     tl.store(c + offset, out, mask=mask)
 
 
