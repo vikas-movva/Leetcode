af7399362f46cd8d4f4217096040b841fe8acafc
Vikas Movva <vikas.s.movva@gmail.com>
2026-08-31 20:44:16 -0400

    Refactor containsNearbyDuplicate method to remove unnecessary nested function

--- python/219.contains-duplicate-ii.py
+++ python/219.contains-duplicate-ii.py
@@ -7,11 +7,10 @@
 # @lc code=start
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        def hash_set():
-            dupes = {}
-            for i, num in enumerate(nums):
-                if num in dupes and abs(dupes[num] - i) <= k:
-                    return True
-                dupes[num] = i
-            return False
+        dupes = {}
+        for i, num in enumerate(nums):
+            if num in dupes and abs(dupes[num] - i) <= k:
+                return True
+            dupes[num] = i
+        return False
 # @lc code=end
